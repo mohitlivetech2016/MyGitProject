66f2089db0b471870e0fe3e7303abb26b2ac3d62
mohit singh <mohit singh>
2016-10-26 07:22:51 +0530

    Initial commit project

---
+++ app/src/main/java/firstproject/mygitproject/MainActivity.java
@@ -0,0 +1,26 @@
+package firstproject.mygitproject;
+
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void add(){
+
+        int a;
+        int b;
+       System.out.print("hello");
+    }
+
+
+
+
+}
